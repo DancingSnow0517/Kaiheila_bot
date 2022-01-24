dc2b92bddbc7f10e858f8a284e72c5ef4bcc0900
DancingSnow <1121149616@qq.com>
2022-01-24 11:23:33 +0800

    更改log样式

--- src/bot.py
+++ src/bot.py
@@ -24,7 +24,8 @@ class KaiheilaBot(Bot):
         self.config = config
         self.cb_client = KaiheilaClient(self)
         self.client.register(MessageTypes.TEXT, self.on_text_msg)
-        logging.basicConfig(level=config.log_level)
+        logging.Formatter(fmt='%(asctime)s', datefmt='%H:%M:%S')
+        logging.basicConfig(level=config.log_level, format='[%(asctime)s] [%(module)s] [%(threadName)s/%(levelname)s]: %(message)s')
         commands.register(self, config.prefixes, config)
 
         # Chromium 相关
