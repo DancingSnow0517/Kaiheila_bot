6b37e302bc6862e3da9296555d063cce62c1c3f6
Huaji_MUR233 <huajimur@jvav.me>
2022-01-29 18:00:59 +0800

    hopefully it works

--- src/utils/cb_client.py
+++ src/utils/cb_client.py
@@ -8,14 +8,6 @@ if TYPE_CHECKING:
     from ..bot import KaiheilaBot
 
 
-def sync(func: Awaitable):
-    try:
-        loop = asyncio.get_event_loop()
-    except:
-        loop = asyncio.new_event_loop()
-    return loop.run_until_complete(func)
-
-
 class KaiheilaClient(ChatBridgeClient):
     def __init__(self, bot: 'KaiheilaBot'):
         config = bot.config
@@ -25,4 +17,4 @@ class KaiheilaClient(ChatBridgeClient):
 
     def on_chat(self, sender: str, payload: ChatPayload):
         channel = sync(self.bot.client.fetch_public_channel(self.config.khl_channel_mc_chat))
-        sync(channel.send("[{}]<{}> {}".format(sender, payload.author, payload.message)))
+        asyncio.run(channel.send("[{}]<{}> {}".format(sender, payload.author, payload.message)))
